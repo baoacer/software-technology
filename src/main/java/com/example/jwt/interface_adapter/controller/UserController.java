0f7363586937d17d97d46451ec7749d02f4d0cb3
baoacer <nguyenquocbaotu@gmail.com>
2025-03-18 23:04:35 +0700

    Complete Feature Update User Info

--- src/main/java/com/example/jwt/interface_adapter/controller/UserController.java
+++ src/main/java/com/example/jwt/interface_adapter/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.example.jwt.interface_adapter.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.jwt.application.usecase.user.UpdateUserInfoUseCase;
+import com.example.jwt.dto.model.UserDto;
+import com.example.jwt.dto.request.UpdateUserRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/user")
+@RestController
+@CrossOrigin(origins = "${cors.allowed-origins}")
+@AllArgsConstructor
 public class UserController {
+
+    private UpdateUserInfoUseCase updateUserInfoUseCase;
+
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(
+            @RequestParam int userId,
+            @RequestBody UpdateUserRequest request) {
+        return new ResponseEntity<>(this.updateUserInfoUseCase.execute(userId, request), HttpStatus.OK);
+    }
 
 }
